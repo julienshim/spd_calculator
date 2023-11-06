c0581d8d89eed8f3a002eb573acfb471e1d384ec
julienshim <25723428+julienshim@users.noreply.github.com>
2023-11-05 22:43:24 -0800

    add calculations

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
 from requests import get
 from bs4 import BeautifulSoup
-from random import randint
 from time import sleep
+from random import randint
+from csv import writer
+from json import dumps
+from re import match
+from datetime import datetime
 
+
+setting_storage_option_cost = True
+
+while setting_storage_option_cost:
+    storage_option_cost = input('How much did your drive storage option cost? (USD, no commas) ')
+    if not match(r'^\d+(\.\d{2})?$', storage_option_cost):
+        print('Hint: USD, no commas, e.g. 1799.99')
+    else:
+        setting_storage_option_cost = False
+
+setting_storage_option_slot_count = True
+
+while setting_storage_option_slot_count:
+    storage_option_slot_count = input('How many drive slots does your storage option have? (no commas) ')
+    if not match(r'^\d+$', storage_option_slot_count):
+        print('Hint: no commas, pick a number between 1-100 unless you have serious data hoarding issues')
+    else:
+        setting_storage_option_slot_count = False
 
 url = 'https://serverpartdeals.com/collections/manufacturer-recertified-drives'
 
@@ -89,8 +111,47 @@ def parse_product(product):
     }
     return product_parsed
 
+def add_calculations(product):
+    price = product['price']
+    capacity = product['capacity'].lower()
+    if 'tb' not in capacity:
+        raise ValueError('Why is capacity not in TB?')
+    capacity = capacity.replace('tb', '')
+    price_per_terrabyte = str(round(eval(f'{price}/{capacity}'),2))
+    price_per_slot = str(round(eval(f'({price}+({storage_option_cost}/{float(storage_option_slot_count)}))/{float(capacity)}'), 2))
+    product_calculated = {
+        **product,
+        'price_per_terrabyte': price_per_terrabyte,
+        'price_per_slot': price_per_slot
+    }
+    return product_calculated
+
+def fetch_header_row(products):
+    header = []
+    for product in products:
+        keys = product.keys()
+        for key in keys:
+            if key not in header:
+                header.append(key)
+    return header
+
+# with open('./sample.html') as f:
+#     page = f.read()
 page = get(url, params=payload)
 soup = BeautifulSoup(page.text, 'html.parser')
 products = soup.find_all('div', class_='boost-pfs-filter-product-item')
 products = list(map(parse_product, products))
+products = list(map(add_calculations, products))
+products.sort(key=lambda x: x['price_per_slot'])
+print('Recommeonded Purchase (based on cost per slot):')
+print(dumps(products[0], indent=4))
 
+## TO-DO: UPDATE WITH TODAYS DATE AND TIME
+
+with open('results.tsv', 'w') as f:
+    csv_writer = writer(f, delimiter='\t')
+    header_row = fetch_header_row(products)
+    csv_writer.writerow(header_row)
+    for product in products:
+        product_row = [product[h] if h in product else None for h in header_row]
+        csv_writer.writerow(product_row)
